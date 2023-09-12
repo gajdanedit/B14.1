21867c0123873343a0fd260259c9f117a95f43f4
gajdanedit <gajdanedit1129@gmail.com>
2023-09-12 08:54:41 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int szamA, szamB;
+            Console.WriteLine("Írj be egy számot!");
+            szamA = int.Parse(Console.ReadLine());
+            Console.WriteLine("Írj be egy másik számot!");
+            szamB = int.Parse(Console.ReadLine());
+            Console.WriteLine("A szám összege:" +(szamA+szamB).ToString());
+
+
+            Console.ReadLine();
+        }
+    }
+}
